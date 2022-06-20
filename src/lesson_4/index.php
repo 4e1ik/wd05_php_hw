91a5f51822c2f38c195048425d7e745db9c18de5
4e1ik <Artemi324@tut.by>
2022-06-20 21:44:47 +0300

    init(lesson_4): add lesson 4

---
+++ src/lesson_4/index.php
@@ -0,0 +1,148 @@
+<?php
+
+    // $post = [
+    //     'title' => 'Моя первая страница',
+    //     'content' => 'Какой-нибудь текст',
+    //     'picture' => "https://www.sunhome.ru/i/wallpapers/163/alberta-banf-kanada.1920x1200.jpg",
+    // ];
+
+    // $i = 1000000000;
+    // $value = 99;
+    // $counter = 0;
+    // while ($value <= $i){
+    //     $value *= 2;
+    //     $counter++;
+    //     echo($counter." ".$value);
+    //     echo "<br>";
+    //     echo "--------------";
+    //     echo "<br>";
+    // }
+
+    // var_dump("final: ".$value." ".$i);
+
+
+    // $i = 1000000000;
+    // $value = 99;
+    // $counter = 0;
+    // while ($value <= $i){
+    //     $value *= 2;
+    //     $counter++;
+    //     echo($counter." ".$value);
+    //     if ($value === 202752){
+    //         break;
+    //     }
+    //     echo "<br>";
+    //     echo "--------------";
+    //     echo "<br>";
+    // }
+
+    // var_dump("final: ".$value." ".$i);
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    // $arr = [];
+    // $value = 0;
+    
+    // while ($value <= 100) {
+    //     $arr[] += $value;
+    //     $value += 2;
+    // }
+
+    // echo "<pre>";
+    // print_r($arr);
+    // echo "<pre>";
+
+    //////////////////////////////////////////////////////////////////////////
+
+    // $array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // $chet = [];
+    // $nech = [];
+
+    // foreach ($array as $key => $el){
+    //     if ($el % 2 == 0){
+    //         $chet[] += $el;
+    //     } else {
+    //         $nech[] += $el;
+    //     }
+
+        
+    // }
+
+    // echo "<pre>";
+    // print_r($chet);
+    // echo "<pre>";
+
+    // echo "<pre>";
+    // print_r($nech);
+    // echo "<pre>";
+
+    //////////////////////////////////////////////////////////////////
+
+    // $array = [
+    //     'ivanov', 'petrov', 'sidorov',
+    // ];
+
+    // foreach ($array as $key => $value) {
+    //     echo "$key => $value <br>";
+    // }
+
+    //////////////////////////////////////////////////////////////////
+
+    // $rand = [];
+
+    // for ($i=0; $i < 10; $i++) { 
+    //     $rand[] = rand (0, 100);    
+    // }
+
+    // echo "<pre>";
+    // print_r($rand);
+    // echo "<pre>";
+
+    /////////////////////////////////////////////////////////////////
+
+    // $posts = [
+    //     [
+    //         'title' => ' Моя первая страница 1',
+    //         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         'img' => 'https://www.kv.by/sites/default/files/pictures/mainimage/2017/05/v.jpg'
+    //     ],
+    //     [
+    //         'title' => ' Моя первая страница 2',
+    //         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         'img' => 'https://www.kv.by/sites/default/files/pictures/mainimage/2017/05/v.jpg'
+    //     ],
+    //     [
+    //         'title' => ' Моя первая страница 3',
+    //         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         'img' => 'https://www.kv.by/sites/default/files/pictures/mainimage/2017/05/v.jpg'
+    //     ],
+    //     [
+    //         'title' => ' Моя первая страница 4',
+    //         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         'img' => 'https://www.kv.by/sites/default/files/pictures/mainimage/2017/05/v.jpg'
+    //     ],
+    //     [
+    //         'title' => ' Моя первая страница 5',
+    //         'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    //         'img' => 'https://www.kv.by/sites/default/files/pictures/mainimage/2017/05/v.jpg'
+    //     ]
+    // ];
+
+
+    // foreach ($posts as $post) {
+    //     echo "<pre>";
+    //     print_r($post);
+    //     echo "<pre>";
+
+    //     foreach ($post as $key => $item) {
+    //         echo $key.":".$item."<br>";
+    //     }
+    // }
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    for ($i=1; $i < 6; $i++){ 
+        echo "$i <br>";
+    }
+    
+?>
