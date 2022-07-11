f743435b5d7b55f1e56a32123ed9f4a55c59fbfc
4e1ik <Artemi324@tut.by>
2022-07-11 17:00:39 +0300

    init(hw-4) add list

--- src/test.php
+++ src/test.php
@@ -2,15 +2,25 @@
 
     include_once 'db.php';
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $title = $_POST['title'];
-        $content = $_POST['content'];
         $filePath = '';
+//        mkdir('uploads'); //Создал папку uploads
         if (!empty($_FILES) && isset($_FILES['img'])){
-            $filePath = "img/".$_FILES['img']['name'];
-            move_uploaded_file($_FILES['img']['tmp_name'], __DIR__."/".$filePath);
+            foreach ($_FILES as $FILE){
+                $name = $_FILES['img']['name'];
+                $filePath = "uploads\\".$name;
+                $res1 = mysqli_query($connection, "select img from posts where img like '(_)$name' or img = '$name';");
+                $sql2 = mysqli_fetch_all($res1);
+                if (!empty($sql2)) {
+                    $i = count($sql2);
+                    $name = "($i)" . $name;
+                    $filePath = "uploads\\" . $name;
+                }
+                $dir = '\\wsl$\Ubuntu-22.04\home\artemi\projects\wd05\src';
+                move_uploaded_file('\\wsl$\docker-desktop-data\data\docker\tmp\\'.$_FILES['img']['name'], $dir."\\".$filePath);
+                $sql1 = "INSERT INTO posts (img) VALUES ('$filePath');";
+                mysqli_query($connection, $sql1);
+            }
         }
-        $sql1 = "INSERT INTO posts (title, content, img) VALUES ('$title', '$content', '$filePath');";
-        mysqli_query($connection, $sql1);
     }
 
 
@@ -21,5 +31,9 @@ echo "<pre>";
 echo "<pre>";
 print_r($_FILES);
 echo "<pre>";
+
+
+
+
 
 ?>
