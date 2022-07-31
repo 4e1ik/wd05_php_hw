db6a857830bac181999d1de4ffd23ffdd7103101
4e1ik <Artemi324@tut.by>
2022-07-31 18:26:39 +0300

    init(hw-6): fix2

---
+++ src/hw-6/Driver.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Driver extends Worker4 {
+    private $drivingExperience;
+    private $drivingCategory;
+
+    public function __construct($name, $age, $salary, $drivingExperience, $drivingCategory){
+        parent::__construct($name, $age, $salary);
+        $this->drivingExperience = $drivingExperience;
+        $this->drivingCategory = $drivingCategory;
+    }
+
+    
+}
---
+++ src/hw-6/Worker4.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class Worker4 extends User {
+    private $salary;
+
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+
+    public function setSalary($salary): void
+    {
+        $this->salary = $salary;
+    }
+
+    public function __construct($name, $age, $salary){
+        $this->name = $name;
+        $this->age= $age;
+        $this->salary = $salary;
+    }
+}
+
+
+?>
